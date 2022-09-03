4603927de88c229ef50230ff3361536f1562fd3c
Koichi.Shimomura <unieye51@gmail.com>
2022-09-03 16:25:55 +0900

    9-39 マイグレーションのインデックスの下準備

--- database/migrations/2022_08_31_171259_add_something_in_books.php
+++ database/migrations/2022_08_31_171259_add_something_in_books.php
@@ -14,31 +14,31 @@ return new class extends Migration
     public function up()
     {
         Schema::table('books', function (Blueprint $table) {
-            // string型（デフォルト255, 第2引数にカラムの長さを指定します）
-            $table->string('name', 100);
-            $table->string('name');
-            // テキスト型
-            $table->text('description');
-            // idを指定します。autoincrementで、bigintのunsignedになります
-            $table->id();
-            // int型（tinyint、int、small、big）
-            $table->tinyInteger('status_code');
-            $table->integer('status_code');
-            $table->smallInteger('status_code');
-            $table->bigInteger('status_code');
-            // created_atとupdated_atが追加されます
-            $table->timestamps();
-            // boolean型（true/false）
-            $table->boolean('is_active');
-            // char型（第2引数にカラムの長さを指定します）
-            $table->char('name', 100);
-            $table->char('name'); // length指定していない場合は、255
-            // dateTime型
-            $table->dateTime('sending_at');
-            // date型
-            $table->date('created_at');
-            // ソフトデリート
-            $table->softDeletes();
+//            // string型（デフォルト255, 第2引数にカラムの長さを指定します）
+//            $table->string('name', 100)->comment('コメントです');
+//            $table->string('name')->default('test');
+//            // テキスト型
+//            $table->text('description')->after('id');
+//            // idを指定します。autoincrementで、bigintのunsignedになります
+//            $table->id();
+//            // int型（tinyint、int、small、big）
+//            $table->tinyInteger('status_code')->autoIncrement();
+//            $table->integer('status_code')->default(1);
+//            $table->smallInteger('status_code')->nullable();
+//            $table->bigInteger('status_code')->unsigned();
+//            // created_atとupdated_atが追加されます
+//            $table->timestamps();
+//            // boolean型（true/false）
+//            $table->boolean('is_active');
+//            // char型（第2引数にカラムの長さを指定します）
+//            $table->char('name', 100);
+//            $table->char('name'); // length指定していない場合は、255
+//            // dateTime型
+//            $table->dateTime('sending_at');
+//            // date型
+//            $table->date('created_at');
+//            // ソフトデリート
+//            $table->softDeletes();
         });
     }
 
